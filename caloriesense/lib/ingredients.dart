69df2d2208aa88a367bceff410ca2737c2cbd363
Silviu <silviuandrei1056@gmail.com>
2024-09-06 15:46:52 +0300

    ingrediente update

--- caloriesense/lib/ingredients.dart
+++ caloriesense/lib/ingredients.dart
@@ -10,13 +10,43 @@ class Ingredients extends StatelessWidget {
       drawer: const Sidebar(),
       appBar: AppBar(
         title: const Text('Ingredientele Mele'),
-        backgroundColor: Colors.blue, // Set the color of the AppBar
+        backgroundColor: Colors.lightBlue,
       ),
-      backgroundColor:
-          Colors.lightBlue[50], // Set the background color of the Scaffold
-      body: const Center(
-        child: Text('Ingredients Page', style: TextStyle(fontSize: 30)),
-      ),
+      backgroundColor: const Color.fromARGB(255, 231, 233, 235),
+      body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 35),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 15,
+              ),
+              searchBox(),
+              ListView()
+            ],
+          )),
     );
   }
+}
+
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+    decoration: BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(30)),
+    child: Row(
+      children: [
+        Expanded(
+            child: TextField(
+          decoration: InputDecoration(
+              hintText: "Cauta ingredientele",
+              hintStyle: TextStyle(color: Colors.black54),
+              border: InputBorder.none),
+        )),
+        Icon(
+          Icons.search,
+          color: Colors.black54,
+        )
+      ],
+    ),
+  );
 }
